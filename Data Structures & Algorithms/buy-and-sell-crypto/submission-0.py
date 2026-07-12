aa0f68fc8243dec4fedf7c4b3fa758505c0364a7
Arjun Karthikeya Nakka <arjunkarthikeya2002@gmail.com>
2026-07-12 16:35:40 -0400

    update buy and sell with complexities

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,5 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # sliding window, T = O(N), S = O(1)
+        # you slide through the array, update the min as you go through and maximize your difference 
+        # of curr el - global min at every index.
         n=len(prices)
         if n==1:
             return 0
