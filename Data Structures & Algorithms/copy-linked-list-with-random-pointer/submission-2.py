d1c90df40e41032e3e375fae8761a019c47aa2d7
Arjun Karthikeya Nakka <70424303+arjunkarthikeyanakka@users.noreply.github.com>
2026-07-19 13:23:33 -0400

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.py
@@ -0,0 +1,72 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # optimal time wise, but space can be optimized even more. 
+        # T = O(n), S = O(n)
+        '''
+        th=Node(0)
+        h=th
+        temp=head
+        idx=0
+        rd_idx,b=defaultdict(int),[]
+        while temp:
+            th.next=Node(temp.val,None,None)
+            th=th.next
+            rd_idx[temp]=idx
+            b.append(th)
+            idx+=1
+            temp=temp.next
+        x,y=h.next,head
+        while x:
+            x.random=b[rd_idx[y.random]] if y.random else None
+            x=x.next
+            y=y.next
+        return h.next
+        '''
+        if head is None:
+            return head
+        th=Node(0)
+        h=th
+        temp=head
+        while temp:
+            temp.next=Node(temp.val,temp.next,None)
+            temp=temp.next.next
+        temp=head
+        while temp and temp.next:
+            copy_node=temp.next
+            copy_node.random=temp.random.next if temp.random else temp.random
+            temp=temp.next.next
+        temp=head
+        th=temp.next
+        while temp:
+            copy_node=temp.next
+            nn=temp.next.next
+            temp.next=nn
+            if copy_node.next and copy_node.next.next:
+                copy_node.next=nn.next
+            temp=nn
+        
+        return th
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
