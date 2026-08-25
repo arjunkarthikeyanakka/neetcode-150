591cbf3b2b65076ae06c60eb49356d8e6dc8509b
Arjun Karthikeya Nakka <70424303+arjunkarthikeyanakka@users.noreply.github.com>
2026-08-25 15:53:18 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,36 @@
+'''
+    Since constraints mention that each set has time strictly greater than previous one, we can 
+    do a simple binary search but there are 2 corner cases, one is if the ts given is lower than 
+    the lowest ts stored, then return "" and if the final left value is greater than ts lower by one idx
+    and then return the ans
+    T = O(logN) where N is the #set operations in total
+    S = O(1) amortized
+'''
+class TimeMap:
+
+    def __init__(self):
+        self.d=defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.d:
+            self.d[key]=[]
+        self.d[key].append((timestamp,value))
+
+    def get(self, key: str, ts: int) -> str:
+        nums=self.d.get(key,None)
+        if not nums or nums[0][0]>ts:
+            return ""
+        # print(nums)
+        left,right=0,len(nums)-1
+        while left<right:
+            mid=left+(right-left)//2
+            if nums[mid][0]==ts:
+                return nums[mid][1]
+            elif nums[mid][0]<ts:
+                left=mid+1
+            else:
+                right=mid
+        # print('end',left,right)
+        if nums[left][0]>ts:
+            left=max(0,left-1)
+        return nums[left][1]
